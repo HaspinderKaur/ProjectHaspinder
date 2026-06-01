2ec118ad14d6c0e5695cd197e65913433983542b
Haspinder Dhillon <kauhaspi@sheridancollege.ca>
2026-06-01 13:06:26 -0400

    This is my Initial Commit

---
+++ src/main/java/com/mycompany/projecthaspinder/ProjectHaspinder.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projecthaspinder;
+
+/**
+ *
+ * @author hash3
+ */
+public class ProjectHaspinder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    
+        System.out.println("This is my In class Exercise for Git Workflow.");
+    
+    }
+    
+    
+   
+}
